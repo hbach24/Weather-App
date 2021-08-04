710797ca235a55c279e340a24e5b0d8f51ee8a5c
hbach24 <57569284+hbach24@users.noreply.github.com>
2021-08-04 16:46:08 -0700

    update style

--- app.js
+++ app.js
@@ -7,22 +7,19 @@ let icon = document.getElementById("icon");
 let weather = document.querySelector(".weather-container");
 // let iconURL = "http://openweathermap.org/img/wn/" + data.weather[0].icon;
 
-button.addEventListener("click", function (e) {
+button.addEventListener("click", function () {
+  weather.classList.remove("hide");
+
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${userInput.value}&units=imperial&appid=cabe0ceec471b071308cc1a6a5681c3c`
   )
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
-
       let iconURL =
         "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
       icon.innerHTML = `<img src="${iconURL}"/>`;
       loc.textContent = data.name;
       temp.textContent = data.main.temp + " °F";
       caption.textContent = data.weather[0].description;
-      //   weather.classList.remove(".hide");
     });
-
-  //  .catch(() => alert("Not a city name."));
 });
