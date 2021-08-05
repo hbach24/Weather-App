595fb0814c20d20bb0401192a8b40c7e5182c83e
hbach24 <57569284+hbach24@users.noreply.github.com>
2021-08-04 17:41:40 -0700

    added geolocation

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const button = document.querySelector(".btn");
 const userInput = document.querySelector(".user-input");
 let loc = document.querySelector(".location");
@@ -5,21 +7,49 @@ let temp = document.querySelector(".degrees");
 let caption = document.querySelector(".caption");
 let icon = document.getElementById("icon");
 let weather = document.querySelector(".weather-container");
-// let iconURL = "http://openweathermap.org/img/wn/" + data.weather[0].icon;
+
+const apiKey = "cabe0ceec471b071308cc1a6a5681c3c";
+let lat;
+let long;
+let iconURL;
+
+function setWeather(data) {
+  iconURL = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
+  icon.innerHTML = `<img src="${iconURL}"/>`;
+  loc.textContent = data.name;
+  temp.textContent = Math.floor(data.main.temp) + " °F";
+  caption.textContent = data.weather[0].description;
+}
+
+window.addEventListener("load", function () {
+  if (this.navigator.geolocation) {
+    this.navigator.geolocation.getCurrentPosition((position) => {
+      lat = position.coords.latitude;
+      long = position.coords.longitude;
+      console.log(lat, long);
+      weather.classList.remove("hide");
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&appid=${apiKey}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setWeather(data);
+        });
+    }); //end of position
+  } //end of if statement
+  else {
+    loc.textContent = "Geolocation unavailable.";
+  }
+});
 
 button.addEventListener("click", function () {
   weather.classList.remove("hide");
 
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${userInput.value}&units=imperial&appid=cabe0ceec471b071308cc1a6a5681c3c`
+    `https://api.openweathermap.org/data/2.5/weather?q=${userInput.value}&units=imperial&appid=${apiKey}`
   )
     .then((response) => response.json())
     .then((data) => {
-      let iconURL =
-        "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
-      icon.innerHTML = `<img src="${iconURL}"/>`;
-      loc.textContent = data.name;
-      temp.textContent = data.main.temp + " °F";
-      caption.textContent = data.weather[0].description;
+      setWeather(data);
     });
 });
